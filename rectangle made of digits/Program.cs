38c9b420b3f4b0292ff50daf1678588dbf00bf1a
Kristen Shea <kstar61@live.com>
2019-04-02 18:16:37 -0500

    Add project files.

---
+++ rectangle made of digits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rectangle_made_of_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
