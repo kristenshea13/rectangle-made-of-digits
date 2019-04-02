8192984545b88b634ba5d5fdc97b635411967712
Kristen Shea <kstar61@live.com>
2019-04-02 18:23:57 -0500

    .

--- rectangle made of digits/Program.cs
+++ rectangle made of digits/Program.cs
@@ -6,7 +6,22 @@ namespace rectangle_made_of_digits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Build a rectangle.");
+            Console.WriteLine("What integer would you like to use? ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}{0}{0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0} {0}", number);
+            Console.WriteLine("{0}{0}{0}", number);
+
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
